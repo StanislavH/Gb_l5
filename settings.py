c8ad69a986ffc572914f9e3a35a4fd3b67391a20
StanislavH <HaukkaStas@gmail.com>
2021-09-20 09:30:06 +0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+SPIDER_MODULES = ['jobparser.spiders']
+NEWSPIDER_MODULE = 'jobparser.spiders'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
+ROBOTSTXT_OBEY = False
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG'  #INFO ERROR
